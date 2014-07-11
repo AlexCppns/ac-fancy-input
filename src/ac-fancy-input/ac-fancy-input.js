a737c7972cdb1cfd2e605d9a4d15baa3b56e2cab
Alexandre Coppens <alexcppns@gmail.com>
2014-07-11 11:15:55 -0400

    tagging new version

--- src/ac-fancy-input/ac-fancy-input.js
+++ src/ac-fancy-input/ac-fancy-input.js
@@ -4,12 +4,6 @@
 // Purpose: directive to create an advanced typeahead combined with an animated search input
 //
 
-
-
-// Create all modules and define dependencies to make sure they exist
-// and are loaded in the correct order to satisfy dependency injection
-// before all nested files are concatenated by Grunt
-
 // Modules
 var acfi = angular.module('ac-fancy-input',[]);
 
